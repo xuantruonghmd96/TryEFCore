43c21762106b59afa5b5c52fb14a84f095f7592f
Nguyễn Đào Xuân Trường <nguyendaoxuantruong@secure.janeto.com>
2019-10-30 10:00:03 +0700

    init

---
+++ Try_LazyLoad_EagerLoad/Models/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Try_LazyLoad_EagerLoad.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
